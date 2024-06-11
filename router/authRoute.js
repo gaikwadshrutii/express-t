435f7a967f08334d109a408662142fc8b31a9ac7
gaikwadshrutii <gaikwadshrutii01@gmail.com>
2024-06-11 13:49:29 +0530

    initial setup

--- router/authRoute.js
+++ router/authRoute.js
@@ -3,6 +3,6 @@ const { registerUser, loginUser } = require("../controller/authController")
 const route = require("express").Router()
 
 route
-.post("/register", registerUser)
-.post("/login", loginUser)
- module.exports = route
+    .post("/register", registerUser)
+    .post("/login", loginUser)
+module.exports = route
